ea413a14b261886a895dd95db7d61f0a16be876e
Chhabra <manila.chhabra28@gmail.com>
2024-08-09 20:48:52 +0530

    Arrow functions

---
+++ ArrowFunnctions.js
@@ -0,0 +1,12 @@
+ product = (a,b)=> {
+    return a +b;
+ }
+
+  let result = product(2,4)
+  console.log('result value of product is = ' + result)
+/// 2nd way
+    product = (a) => 10 + 10 //or 
+  product = () => 10 + 10
+  let result1 = product(2,4)
+  console.log('result value of product is = ' + result1)
+
