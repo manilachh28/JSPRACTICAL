10809e19b5df6e51dc4fb03fb34077e44784b63c
Chhabra <manila.chhabra28@gmail.com>
2024-08-09 12:09:13 +0530

    my first code of practice JS

---
+++ Basics.js
@@ -0,0 +1,10 @@
+
+// Variables
+let age = 30
+age = 'thirty' // to update the value of age
+console.log('Age is : ' +age)
+
+//Constants 
+const age1 = 30
+// age = 'thirty' (never update the value of const)
+console.log('Age is : ' +age1)
