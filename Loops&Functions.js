aca4ed641861ccabfd5d1f0bf00c68732a9c2606
Chhabra <manila.chhabra28@gmail.com>
2024-08-09 12:11:41 +0530

    Third file

---
+++ Loops&Functions.js
@@ -0,0 +1,20 @@
+let i=0
+while (i<5)
+{
+    console.log('i is = ' +i)
+    i++;
+}
+
+
+for(i=0; i<5 ; i++)
+{
+    console.log('i is = ' +i)
+}
+
+// Functions
+ function product (a,b){
+    return a +b;
+ }
+
+  let result = product(2,4)
+  console.log('result value of product is = ' + result)
