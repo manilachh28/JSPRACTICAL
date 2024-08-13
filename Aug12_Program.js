6956586fee38d53d2282136bf435d08756733096
Chhabra <manila.chhabra28@gmail.com>
2024-08-13 21:55:33 +0530

    Resolved prompt issue

--- Aug12_Program.js
+++ Aug12_Program.js
@@ -1,6 +1,6 @@
 const prompt = require("prompt-sync")();
-var base = prompt('Please enter the base value');
-var height =  prompt('Please enter the height value');
+var base = prompt('Please enter the base value = ');
+var height =  prompt('Please enter the height value = ');
 var area = (base*height)/2;
 console.log('area of triangle is = ' +area);
 
