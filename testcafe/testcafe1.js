7015b089ba206bee3f70424a2281924a2d16ecc4
Chhabra <manila.chhabra28@gmail.com>
2024-08-20 19:47:36 +0530

    Update package after testcase and typescript

---
+++ testcafe/testcafe1.js
@@ -0,0 +1,12 @@
+fixture('Getting Started')
+    .page('https://www.google.co.in');
+
+test('First testCafeTest', async t => {
+await t.typeText('#APjFqb', 'TestCafe')
+await t.click('.gNO89b')
+await t.wait(2000)
+});
+
+test('Second testCafeTest', async t => {
+    
+})
